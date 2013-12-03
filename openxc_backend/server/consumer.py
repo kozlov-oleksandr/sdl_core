735315ce1afa56a37c3cc583cbe09c2465c88eb1
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-03 10:27:10 +0200

    Add new python test modules to the openxc backend dir

---
+++ openxc_backend/server/consumer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+"""
+An HTTP & web socket server that reads from a TLS server stream and writes all
+data to connected web socket clients.
+"""
+
+import os
+import logging
+import socket
+from datetime import datetime
+
+from tornado.web import Application, RequestHandler, asynchronous
+from tornado.websocket import WebSocketHandler
+from tornado.iostream import SSLIOStream
+from tornado.ioloop import IOLoop
+from tornado import options
+
+from lib.stream_reader import SSLStreamReader
+
+
+log = logging.getLogger(__name__)
+port = 8080
+tls_host = 'localhost'
+tls_port = 4443
+
+
+class CustomReader(SSLStreamReader):
+    @staticmethod
+    def process(data):
+        return "%s - %s" % (datetime.now(), str(data))
+
+    @staticmethod
+    def write_message(client, data):
+        client.write_message(data)
+
+
+class RootHandler(RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class FeedHandler(WebSocketHandler):
+    feed = CustomReader(tls_host, tls_port)
+
+    def open(self):
+        FeedHandler.feed.add(self)
+
+    def on_close(self):
+        FeedHandler.feed.remove(self)
+
+
+settings = {
+    'template_path': os.path.join(os.path.dirname(__file__), 'html')
+}
+routes = [
+    (r'/', RootHandler),
+    (r'/feed', FeedHandler)
+]
+
+if __name__ == "__main__":
+    options.parse_command_line()
+    application = Application(routes, **settings)
+    application.listen(port)
+    log.info('Listening on port %d...', port)
+    IOLoop.instance().start()
---
+++ openxc_backend/server/lib/stream_reader.py
@@ -0,0 +1,56 @@
+import logging
+import socket
+
+from tornado.iostream import SSLIOStream
+from tornado.gen import coroutine, Task
+
+
+log = logging.getLogger(__name__)
+
+
+class SSLStreamReader(object):
+    def __init__(self, host, port):
+        self._address = (host, port)
+        self._stream = None
+        self._clients = set()
+
+    def add(self, client):
+        self._clients.add(client)
+        log.debug('Added client %s', client)
+        if self._stream is None:
+            self._connect()
+            self._read_stream()
+
+    def remove(self, client):
+        self._clients.remove(client)
+        log.debug('Removed client %s', client)
+
+    @staticmethod
+    def process(data):
+        return data
+
+    @staticmethod
+    def write_message(client, data):
+        pass
+
+    def _connect(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self._stream = SSLIOStream(s)
+        log.debug('Connecting to %s', self._address)
+        self._stream.connect(self._address)
+
+    def _disconnect(self):
+        self._stream.close()
+        self._stream = None
+        log.debug('Disconnected from %s', self._address)
+
+    @coroutine
+    def _read_stream(self):
+        log.debug('Reading stream from %s', self._address)
+        while self._clients:
+            data = yield Task(self._stream.read_until, "\n")
+            output = self.process(data)
+            for client in self._clients:
+                self.write_message(client, output)
+        self._disconnect()
+        return
