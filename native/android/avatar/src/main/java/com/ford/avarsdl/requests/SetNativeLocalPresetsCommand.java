4707c0e875d7df0c6807db479c58b8152b58fbe3
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-06 17:57:09 +0200

    REVSDL-121-Implement Radio Station presets local storing (create classes).

---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/requests/SetNativeLocalPresetsCommand.java
@@ -0,0 +1,11 @@
+package com.ford.avarsdl.requests;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/6/13
+ * Time: 1:04 PM
+ */
+public class SetNativeLocalPresetsCommand {
+
+}
---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/responses/GetNativeLocalPresetsResponse.java
@@ -0,0 +1,25 @@
+package com.ford.avarsdl.responses;
+
+import com.ford.avarsdl.jsoncontroller.JSONController;
+import com.ford.avarsdl.util.Logger;
+import com.ford.avarsdl.util.RPCConst;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/6/13
+ * Time: 2:49 PM
+ */
+public class GetNativeLocalPresetsResponse extends JSONController implements ResponseCommand {
+
+    public GetNativeLocalPresetsResponse() {
+        super(RPCConst.CN_REVSDL);
+    }
+
+    @Override
+    public void execute(int id, String result) {
+        Logger.d(getClass().getSimpleName() + " id: " + id + ", result: " + result);
+
+        //sendResponse(id, result);
+    }
+}
