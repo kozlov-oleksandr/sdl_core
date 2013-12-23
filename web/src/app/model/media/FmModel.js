ce61999fec8f6dcbf8360b4e9fe367c2c99f3850
Sergei Polishchuk <SPolishchuk@luxoft.com>
2013-12-23 11:24:04 +0200

    Roll back to a previous version

--- web/src/app/model/media/FmModel.js
+++ web/src/app/model/media/FmModel.js
@@ -1,8 +1,8 @@
 /**
  * @name MFT.FmData
- * 
+ *
  * @desc FM Media data model
- * 
+ *
  * @category	Model
  * @filesource	app/model/media/FmData.js
  * @version		2.0
@@ -13,76 +13,79 @@ MFT.FmModel = Em.Object.create({
 
     init: function () {
         var i,
-            index,
-            station,
             directTunes = [],
-            directTuneItems = {};
+            directTuneItems = {},
+            frequency = 87,
+            fraction = 9;
 
         this._super();
 
-        for (i = 87.9; i <= 107.9; i+=0.1) {
-            station = i.toFixed(1);
-            index = station.replace('.','');
+        for (i = 879; i <= 1079; i++) {
+            directTunes.push(i.toString().split(''));
+            directTuneItems[i] = MFT.PlaylistItem.create({frequency: frequency+'.'+fraction});
 
-            directTuneItems[index] = MFT.PlaylistItem.create({frequency: station});
-
-            directTunes.push(index.split(''));
+            if (fraction < 9) {
+                fraction++;
+            } else {
+                frequency++;
+                fraction = 0;
+            }
         }
 
         this.directTunestations.set('directTunes', directTunes);
         this.directTunestations.set('directTuneItems', directTuneItems);
     },
-	
-	band: MFT.RangedValue.create({value:-1, activeBand:0}),
-	
-	directTunestations: MFT.Playlist.create({
-		
-			/** Direct tune dial station matrix */
-			directTunes: [],
-			
-			/** Direct tune Data */
-			directTuneItems: {}
-	
-	}),
-	
-	fm1: MFT.Playlist.create( {
-		selectedIndex: 					1,
 
-		items: {
-			0:MFT.PlaylistItem.create({frequency: '87.9',genre: 'Pop',title: 'BlUE SKY',artist: 'THE MAX', isHd: false}) ,
-			1:MFT.PlaylistItem.create({frequency: '90.9',genre: 'Club',title: 'JUMP AND DOWN',artist: 'THE PROJECT X', isHd: false}),
-			2:MFT.PlaylistItem.create({frequency: '105.1',genre: 'Rock',title: 'WELCOME HOME',artist: 'TODD SULLIVAN',isHd: false}),
-			3:MFT.PlaylistItem.create({frequency: '98.5',genre: 'Pop',title: 'LETS DANCE',artist: 'MICHAEL JOHNSON',isHd: false}),
-			4:MFT.PlaylistItem.create({frequency: '106.3',genre: 'Pop Rock',title: 'YESTERDAY NIGHT',artist: 'JOHN SMITH', isHd: false}),
-			5:MFT.PlaylistItem.create({frequency: '107.9',genre: 'Classic',title: 'TENTH SYMPHONY',artist: 'SPENCER M.', isHd: false})
-		}
-	}),
-	
-	fm2: MFT.Playlist.create( {
-		selectedIndex: 					4,
+    band: MFT.RangedValue.create({value:-1, activeBand:0}),
 
-        items:{
-			0:MFT.PlaylistItem.create({frequency: '101.1',genre: 'Club',title: 'SPRING TIME',artist: 'DJ SKY', isHd: false}) ,
-			1:MFT.PlaylistItem.create({frequency: '103.2',genre: 'Rock',title: 'RAINBOW',artist: 'THE BEES', isHd: false}),
-			2:MFT.PlaylistItem.create({frequency: '99.3',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'SUNSET',artist: 'SKYLARK',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 2}),
-			3:MFT.PlaylistItem.create({frequency: '103.5',genre: 'Club',title: 'JUMP AND DOWN',artist: 'THE PROJECT X',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 3}),
-			4:MFT.PlaylistItem.create({frequency: '104.1',genre: 'Pop',title: 'HONEY',artist: 'EPTON JOHN', isHd: false}),
-			5:MFT.PlaylistItem.create({frequency: '105.2',genre: 'Pop',title: 'LETS DANCE',artist: 'MICHAEL JOHNSON', isHd: false})
-		}
-	}),
-	
-	fmAst: MFT.Playlist.create( {				
-		selectedIndex: 					1,
+    directTunestations: MFT.Playlist.create({
+
+        /** Direct tune dial station matrix */
+        directTunes: [],
+
+        /** Direct tune Data */
+        directTuneItems: {}
+
+    }),
+
+    fm1: MFT.Playlist.create( {
+        selectedIndex: 					1,
+
+        items: {
+            0:MFT.PlaylistItem.create({frequency: '87.9',genre: 'Pop',title: 'BlUE SKY',artist: 'THE MAX', isHd: false}) ,
+            1:MFT.PlaylistItem.create({frequency: '90.9',genre: 'Club',title: 'JUMP AND DOWN',artist: 'THE PROJECT X', isHd: false}),
+            2:MFT.PlaylistItem.create({frequency: '105.1',genre: 'Rock',title: 'WELCOME HOME',artist: 'TODD SULLIVAN',isHd: false}),
+            3:MFT.PlaylistItem.create({frequency: '98.5',genre: 'Pop',title: 'LETS DANCE',artist: 'MICHAEL JOHNSON',isHd: false}),
+            4:MFT.PlaylistItem.create({frequency: '106.3',genre: 'Pop Rock',title: 'YESTERDAY NIGHT',artist: 'JOHN SMITH', isHd: false}),
+            5:MFT.PlaylistItem.create({frequency: '107.9',genre: 'Classic',title: 'TENTH SYMPHONY',artist: 'SPENCER M.', isHd: false})
+        }
+    }),
+
+    fm2: MFT.Playlist.create( {
+        selectedIndex: 					4,
 
         items:{
-			0:MFT.PlaylistItem.create({frequency: '98.2',genre: 'Club',title: 'SPRING TIME',artist: 'DJ SKY', isHd: false}) ,
-			1:MFT.PlaylistItem.create({frequency: '106.6',genre: 'Rock',title: 'WELCOME HOME',artist: 'TODD SULLIVAN', isHd: false}),
-			2:MFT.PlaylistItem.create({frequency: '99.3',genre: 'Pop',title: 'GOOD MORNING',artist: 'SUSAN BAKER',isHd: MFT.SettingsModel.isEnglish, HDChannels: 2, currentHDChannel: 2}),
-			3:MFT.PlaylistItem.create({frequency: '107.5',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'SUNSET',artist: 'SKYLARK',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 1}),
-			4:MFT.PlaylistItem.create({frequency: '106.1',genre: 'Pop',title: 'HONEY',artist: 'EPTON JOHN', isHd: false}),
-			5:MFT.PlaylistItem.create({frequency: '104.2',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'TENTH SYMPHONY',artist: 'SPENCER M.', isHd: false})
-		}
+            0:MFT.PlaylistItem.create({frequency: '101.1',genre: 'Club',title: 'SPRING TIME',artist: 'DJ SKY', isHd: false}) ,
+            1:MFT.PlaylistItem.create({frequency: '103.2',genre: 'Rock',title: 'RAINBOW',artist: 'THE BEES', isHd: false}),
+            2:MFT.PlaylistItem.create({frequency: '99.3',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'SUNSET',artist: 'SKYLARK',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 2}),
+            3:MFT.PlaylistItem.create({frequency: '103.5',genre: 'Club',title: 'JUMP AND DOWN',artist: 'THE PROJECT X',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 3}),
+            4:MFT.PlaylistItem.create({frequency: '104.1',genre: 'Pop',title: 'HONEY',artist: 'EPTON JOHN', isHd: false}),
+            5:MFT.PlaylistItem.create({frequency: '105.2',genre: 'Pop',title: 'LETS DANCE',artist: 'MICHAEL JOHNSON', isHd: false})
+        }
+    }),
 
-	})
+    fmAst: MFT.Playlist.create( {
+        selectedIndex: 					1,
+
+        items:{
+            0:MFT.PlaylistItem.create({frequency: '98.2',genre: 'Club',title: 'SPRING TIME',artist: 'DJ SKY', isHd: false}) ,
+            1:MFT.PlaylistItem.create({frequency: '106.6',genre: 'Rock',title: 'WELCOME HOME',artist: 'TODD SULLIVAN', isHd: false}),
+            2:MFT.PlaylistItem.create({frequency: '99.3',genre: 'Pop',title: 'GOOD MORNING',artist: 'SUSAN BAKER',isHd: MFT.SettingsModel.isEnglish, HDChannels: 2, currentHDChannel: 2}),
+            3:MFT.PlaylistItem.create({frequency: '107.5',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'SUNSET',artist: 'SKYLARK',isHd: MFT.SettingsModel.isEnglish, HDChannels: 3, currentHDChannel: 1}),
+            4:MFT.PlaylistItem.create({frequency: '106.1',genre: 'Pop',title: 'HONEY',artist: 'EPTON JOHN', isHd: false}),
+            5:MFT.PlaylistItem.create({frequency: '104.2',genreBinding: 'MFT.locale.label.view_media_genre_classic',title: 'TENTH SYMPHONY',artist: 'SPENCER M.', isHd: false})
+        }
+
+    })
 
 });
